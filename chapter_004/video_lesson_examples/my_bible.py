f23a819fb0be75c02649a007b1e48a158b9ec27f
LogosTeach <jcpartri@logosteach.com>
2026-02-02 16:12:09 -0500

    Add Bible class with book list and version
    This class represents the Protestant Canon of the Bible with a list of books and an instance variable for the version.

---
+++ chapter_004/video_lesson_examples/my_bible.py
@@ -0,0 +1,85 @@
+class Bible:
+    # Protestant Canon
+    books = (     # class variable - shared in the class.
+    "Genesis",
+    "Exodus",
+    "Leviticus",
+    "Numbers",
+    "Deuteronomy",
+    "Joshua",
+    "Judges",
+    "Ruth",
+    "1 Samuel",
+    "2 Samuel",
+    "1 Kings",
+    "2 Kings",
+    "1 Chronicles",
+    "2 Chronicles",
+    "Ezra",
+    "Nehemiah",
+    "Esther",
+    "Job",
+    "Psalms",
+    "Proverbs",
+    "Ecclesiastes",
+    "Song of Solomon",
+    "Isaiah",
+    "Jeremiah",
+    "Lamentations",
+    "Ezekiel",
+    "Daniel",
+    "Hosea",
+    "Joel",
+    "Amos",
+    "Obadiah",
+    "Jonah",
+    "Micah",
+    "Nahum",
+    "Habakkuk",
+    "Zephaniah",
+    "Haggai",
+    "Zechariah",
+    "Malachi",
+    "Matthew",
+    "Mark",
+    "Luke",
+    "John",
+    "Acts",
+    "Romans",
+    "1 Corinthians",
+    "2 Corinthians",
+    "Galatians",
+    "Ephesians",
+    "Philippians",
+    "Colossians",
+    "1 Thessalonians",
+    "2 Thessalonians",
+    "1 Timothy",
+    "2 Timothy",
+    "Titus",
+    "Philemon",
+    "Hebrews",
+    "James",
+    "1 Peter",
+    "2 Peter",
+    "1 John",
+    "2 John",
+    "3 John",
+    "Jude",
+    "Revelation"
+    )
+
+    def __init__(self, version):
+        self.version = version  # instance variable (unique)
+
+# Create an instance of the bible
+my_bible = Bible('KJV')
+
+# Access the attributes of this bible.
+print(f"I have a {my_bible.version}\nThe books in this bible are:")
+for book in my_bible.books:
+    print(f"\t{book}")
+
+"""
+copyright 2026 - LogosTeach - All Rights Reserved
+"""
